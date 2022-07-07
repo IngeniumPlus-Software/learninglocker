2c1a67bf4bb0404afeabd616997045f501f1e6aa
Anna Helton <ahelton@ingeniumplus.com>
2022-07-07 10:12:17 -0400

    change where healthcheck file lives

--- scripts/deploy/app_setup.sh
+++ scripts/deploy/app_setup.sh
@@ -5,7 +5,7 @@ set -e
 cd /var/www/learning-locker-latest
 
 unzip output.zip -d /var/www/learning-locker-latest/
-cp /var/www/learning-locker-latest/health.js /var/www/html/health.js
+cp /var/www/learning-locker-latest/health.js /var/www/learning-locker-latest/ui/dist/public/health.js
 
 #run this as nodejs
 su nodejs
