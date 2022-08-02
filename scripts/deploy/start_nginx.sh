fd727bdf33b36e5259e71e83021c39f59ca6f9f1
Anna Helton <ahelton@ingeniumplus.com>
2022-08-02 15:04:22 -0400

    Update start_nginx.sh

--- scripts/deploy/start_nginx.sh
+++ scripts/deploy/start_nginx.sh
@@ -4,5 +4,6 @@ set -e
 cd ~
 # Update Nginx conf for environment variables
 # Performing this here instead of postcopy script because the conf file is not present when the postcopy runs.
-cat /etc/nginx/conf.d/learning-locker.conf | sed "s/UI_PORT/$UI_PORT/" | sed "s/API_PORT/$API_PORT/" > /var/www/learning-locker-latest/learning-locker.conf & mv -f /var/www/learning-locker-latest/learning-locker.conf /etc/nginx/conf.d/learning-locker.conf
+sed -i "s+UI_PORT+$UI_PORT+g" /etc/nginx/conf.d/learning-locker.conf
+sed -i "s+API_PORT+$API_PORT+g" /etc/nginx/conf.d/learning-locker.conf
 service nginx start
